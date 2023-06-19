c6a5dc2a3ca4902ad71a9477a3af11433287cdef
thedarkness <sthomas25854@protonmail.com>
2023-06-19 14:01:32 -0700

    reviewing events

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const books = [ 
+  {
+    author: 'Jordan Moore',
+    title: 'Interesting Facts For Curious Minds',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 1,
+  },
+  {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  id: 2,
+  },
+]
+
+function BookList() {
+  return ( 
+    <section className="booklist">
+      {books.map((dog) => {
+        const {img, title, author, id} = dog; 
+        return <Book {...dog} key={dog.id} />;
+    })}
+  </section>
+  );
+};
+
+const Book = (props) => {
+  const {img, title, author} = props;
+  return (
+    <article className="book">
+      <img src={img} alt="Intresting facts from curious minds" />
+      <h2>{title}</h2>
+      <h1>{author}</h1>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<BookList />);
---
+++ src/practice_page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+function Practice_page(){ 
+    return (
+         <div>
+            <h2>placeholder</h2>
+        </div>
+);
+};
+
+
+
+export default Practice_page;
