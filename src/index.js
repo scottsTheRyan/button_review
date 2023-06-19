7acd0e33e9aa9f5fec95b14103f4ba87c21d19a0
thedarkness <sthomas25854@protonmail.com>
2023-06-19 14:16:17 -0700

    practice practice practice always be coding

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ const books = [
 function BookList() {
   return ( 
     <section className="booklist">
+      <EventExamples/>
       {books.map((dog) => {
         const {img, title, author, id} = dog; 
         return <Book {...dog} key={dog.id} />;
@@ -28,6 +29,30 @@ function BookList() {
   </section>
   );
 };
+
+const EventExamples = () => {
+  const handleFormInput= () => {
+    console.log('handle form input')
+  };
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+  return (
+    <section>
+      <form>
+        <h2>Reviewing Forms</h2>
+        <input 
+          type='text'
+          name='example'
+          onChange={handleFormInput}
+          style={{margin: '1 rem 0'}}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click Here</button>
+    </section>
+  );
+};
+
 
 const Book = (props) => {
   const {img, title, author} = props;
